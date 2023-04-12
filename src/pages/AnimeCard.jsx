5b9c23d60f583b8cc8c196460bd8c05959dcf9a1
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2023-04-11 21:53:31 -0300

    testing components

--- src/pages/AnimeCard.jsx
+++ src/pages/AnimeCard.jsx
@@ -8,19 +8,19 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Rating from "@mui/material/Rating";
-import Stack from '@mui/material/Stack';
-import Chip from '@mui/material/Chip';
+import Stack from "@mui/material/Stack";
+import Chip from "@mui/material/Chip";
+import CssBaseline from "@mui/material/CssBaseline";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import DirectionsIcon from "@mui/icons-material/Directions";
+import SellIcon from "@mui/icons-material/Sell";
 
 import styles from "../styles/AnimeCard.module.css";
-
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-  >
-    •
-  </Box>
-);
 
 export default function AnimeCard() {
   //https://api.jikan.moe/v4/seasons/now?limit=25&page=1
@@ -32,203 +32,143 @@ export default function AnimeCard() {
         "https://api.jikan.moe/v4/seasons/now?limit=25&page=1"
       );
       const jsonData = await response.json();
+      console.log(jsonData.data);
       setData(jsonData.data);
     }
 
     fetchData();
   }, []);
 
+  /* ------------------------------------------------------------------------- */
+  const [text, setText] = useState("");
+
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+    //console.log(event.target.value); // exibe cada tecla digitada no console
+
+    findAnimesInSearch(event.target.value);
+  };
+
+  function findAnimesInSearch(text) {
+    console.log(data);
+    let animesToShow = data.filter((anime) => anime.title.includes(text));
+    //console.log(animesToShow);
+  }
+
   //console.log(data[2]);
   const numbers = [1, 2, 3, 4, 5];
 
   const value = 3.5;
 
+  const users = [
+    { id: 1, name: "João", age: 25 },
+    { id: 2, name: "Maria", age: 30 },
+    { id: 3, name: "Pedro", age: 20 },
+  ];
+
   return (
     <>
-      {/*     <ul>
-      {numbers.map((number) => (
-        <li key={number}>{number}</li>
-      ))}
-    </ul>
-      <div className={styles.bla}>
-        <Button variant="contained" >Hello World</Button>
-        <Autocomplete
-          disablePortal
-          id="combo-box-demo"
-          options={top100Films}
-          sx={{ width: 300 }}
-          renderInput={(params) => <TextField {...params} label="Movie" />}
+      <Paper
+        component="form"
+        sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+      >
+        <IconButton sx={{ p: "10px" }}>
+          <SellIcon fontSize="small" />
+        </IconButton>
+
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search Animes"
+          value={text}
+          onChange={handleInputChange}
         />
-      </div> */}
 
-      <Card  className={styles.animeCard}>
-          <img
-            className={styles.image}
-            src="https://cdn.myanimelist.net/images/anime/1298/134178l.jpg"
-            alt="anime image"
-          />
-        <div className={styles.animeInformationsContainer}>
+        <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
 
-          <Typography variant="h5" className={styles.title}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, quisquam?
-          </Typography>
+      <ul>
+        {numbers.map((number) => (
+          <li key={number}>{number}</li>
+        ))}
+      </ul>
 
-          <div className={styles.chipsContainer}>
-            <Chip label="Chip Outlined" variant="outlined"/>
-          </div> 
-
-          <div className={styles.ratingContainer}>
-            <Rating name="half-rating-read" value={value} precision={0.1} readOnly /> {/* defaultValue can be better - test after */}
-            <Typography color="text.primary">{value}</Typography>
+      <div>
+        {users.map((user) => (
+          <div key={user.id}>
+            <h2>{user.name}</h2>
+            <p>{user.age} anos</p>
           </div>
+        ))}
+      </div>
 
-          <Typography color="text.secondary" align='justify'>
-            After her boyfriend cheats on her with another girl he met in-game,
-            Akane Kinoshita learns the hard way that gamer boyfriends can be
-            just as bad as the offline variety. As she vents her anger by
-            beating mob monsters to a pulp, she has a chance encounter with
-            Akito Yamada, a guildmate with an afro and Noh mask. Yamada is
-            unsociable and curt, hardly the type of person to make Akane swoon.
-            However, while his gaming stats might be maxed out, Yamada is
-            inexperienced in matters of the heart, and against all odds, she
-            finds herself moved by him. Making new friends along the way, can
-            Akane shake the heart of Yamada, a final boss-level high school boy?
-            [Written by MAL Rewrite]
-          </Typography>
+      <div>
+        {data.map((anime, index) => (
+          <div key={anime.title}>
+            <h1>
+              {index + 1} - {anime.title}
+            </h1>
+          </div>
+        ))}
+      </div>
 
-          <Button className={styles.seeMoreButton} variant="contained">Read More</Button>
-        
-
-        </div>
-      </Card >
+      {data.map((anime, index) => (
+        <AnimeCardComponent anime={data[index]} key={anime.title}/>
+      ))}
     </>
   );
 }
 
-const top100Films = [
-  { label: "The Shawshank Redemption", year: 1994 },
-  { label: "The Godfather", year: 1972 },
-  { label: "The Godfather: Part II", year: 1974 },
-  { label: "The Dark Knight", year: 2008 },
-  { label: "12 Angry Men", year: 1957 },
-  { label: "Schindler's List", year: 1993 },
-  { label: "Pulp Fiction", year: 1994 },
-  {
-    label: "The Lord of the Rings: The Return of the King",
-    year: 2003,
-  },
-  { label: "The Good, the Bad and the Ugly", year: 1966 },
-  { label: "Fight Club", year: 1999 },
-  {
-    label: "The Lord of the Rings: The Fellowship of the Ring",
-    year: 2001,
-  },
-  {
-    label: "Star Wars: Episode V - The Empire Strikes Back",
-    year: 1980,
-  },
-  { label: "Forrest Gump", year: 1994 },
-  { label: "Inception", year: 2010 },
-  {
-    label: "The Lord of the Rings: The Two Towers",
-    year: 2002,
-  },
-  { label: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { label: "Goodfellas", year: 1990 },
-  { label: "The Matrix", year: 1999 },
-  { label: "Seven Samurai", year: 1954 },
-  {
-    label: "Star Wars: Episode IV - A New Hope",
-    year: 1977,
-  },
-  { label: "City of God", year: 2002 },
-  { label: "Se7en", year: 1995 },
-  { label: "The Silence of the Lambs", year: 1991 },
-  { label: "It's a Wonderful Life", year: 1946 },
-  { label: "Life Is Beautiful", year: 1997 },
-  { label: "The Usual Suspects", year: 1995 },
-  { label: "Léon: The Professional", year: 1994 },
-  { label: "Spirited Away", year: 2001 },
-  { label: "Saving Private Ryan", year: 1998 },
-  { label: "Once Upon a Time in the West", year: 1968 },
-  { label: "American History X", year: 1998 },
-  { label: "Interstellar", year: 2014 },
-  { label: "Casablanca", year: 1942 },
-  { label: "City Lights", year: 1931 },
-  { label: "Psycho", year: 1960 },
-  { label: "The Green Mile", year: 1999 },
-  { label: "The Intouchables", year: 2011 },
-  { label: "Modern Times", year: 1936 },
-  { label: "Raiders of the Lost Ark", year: 1981 },
-  { label: "Rear Window", year: 1954 },
-  { label: "The Pianist", year: 2002 },
-  { label: "The Departed", year: 2006 },
-  { label: "Terminator 2: Judgment Day", year: 1991 },
-  { label: "Back to the Future", year: 1985 },
-  { label: "Whiplash", year: 2014 },
-  { label: "Gladiator", year: 2000 },
-  { label: "Memento", year: 2000 },
-  { label: "The Prestige", year: 2006 },
-  { label: "The Lion King", year: 1994 },
-  { label: "Apocalypse Now", year: 1979 },
-  { label: "Alien", year: 1979 },
-  { label: "Sunset Boulevard", year: 1950 },
-  {
-    label:
-      "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
-    year: 1964,
-  },
-  { label: "The Great Dictator", year: 1940 },
-  { label: "Cinema Paradiso", year: 1988 },
-  { label: "The Lives of Others", year: 2006 },
-  { label: "Grave of the Fireflies", year: 1988 },
-  { label: "Paths of Glory", year: 1957 },
-  { label: "Django Unchained", year: 2012 },
-  { label: "The Shining", year: 1980 },
-  { label: "WALL·E", year: 2008 },
-  { label: "American Beauty", year: 1999 },
-  { label: "The Dark Knight Rises", year: 2012 },
-  { label: "Princess Mononoke", year: 1997 },
-  { label: "Aliens", year: 1986 },
-  { label: "Oldboy", year: 2003 },
-  { label: "Once Upon a Time in America", year: 1984 },
-  { label: "Witness for the Prosecution", year: 1957 },
-  { label: "Das Boot", year: 1981 },
-  { label: "Citizen Kane", year: 1941 },
-  { label: "North by Northwest", year: 1959 },
-  { label: "Vertigo", year: 1958 },
-  {
-    label: "Star Wars: Episode VI - Return of the Jedi",
-    year: 1983,
-  },
-  { label: "Reservoir Dogs", year: 1992 },
-  { label: "Braveheart", year: 1995 },
-  { label: "M", year: 1931 },
-  { label: "Requiem for a Dream", year: 2000 },
-  { label: "Amélie", year: 2001 },
-  { label: "A Clockwork Orange", year: 1971 },
-  { label: "Like Stars on Earth", year: 2007 },
-  { label: "Taxi Driver", year: 1976 },
-  { label: "Lawrence of Arabia", year: 1962 },
-  { label: "Double Indemnity", year: 1944 },
-  {
-    label: "Eternal Sunshine of the Spotless Mind",
-    year: 2004,
-  },
-  { label: "Amadeus", year: 1984 },
-  { label: "To Kill a Mockingbird", year: 1962 },
-  { label: "Toy Story 3", year: 2010 },
-  { label: "Logan", year: 2017 },
-  { label: "Full Metal Jacket", year: 1987 },
-  { label: "Dangal", year: 2016 },
-  { label: "The Sting", year: 1973 },
-  { label: "2001: A Space Odyssey", year: 1968 },
-  { label: "Singin' in the Rain", year: 1952 },
-  { label: "Toy Story", year: 1995 },
-  { label: "Bicycle Thieves", year: 1948 },
-  { label: "The Kid", year: 1921 },
-  { label: "Inglourious Basterds", year: 2009 },
-  { label: "Snatch", year: 2000 },
-  { label: "3 Idiots", year: 2009 },
-  { label: "Monty Python and the Holy Grail", year: 1975 },
-];
+function AnimeCardComponent({ anime }) {
+  let image = '';
+  let title = '';
+  let genres = '';
+  let rating = '';
+  let subtitle = '';
+
+
+  if(anime){
+    image = anime.images.jpg.large_image_url;
+    title = anime.title;
+    genres = anime.genres;
+    rating = anime.score;
+    subtitle = anime.synopsis;
+  }
+
+  return (
+    <Card className={styles.animeCard}>
+      <img
+        className={styles.image}
+        src={image}
+        alt="current anime image"
+      />
+      <div className={styles.animeInformationsContainer}>
+        <Typography variant="h5" className={styles.title}>{title}</Typography>
+
+        <div className={styles.chipsContainer}>
+          {genres.map((genre) => (
+            <Chip label={genre.name} variant="outlined" sx={{ mr:0.75 }}/>
+          ))}
+        </div>
+
+        <div className={styles.ratingContainer}>
+          <Rating
+            name="half-rating-read"
+            defaultValue={rating / 2}
+            precision={0.1}
+            readOnly
+          />
+          <Typography color="text.primary">{rating}</Typography>
+        </div>
+
+        <Typography color="text.secondary" align="justify">{subtitle}</Typography>
+
+        <Button className={styles.seeMoreButton} variant="contained">
+          Read More
+        </Button>
+
+      </div>
+    </Card>
+  );
+}
