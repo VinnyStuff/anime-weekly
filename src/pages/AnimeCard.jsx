30f67fa4fb42686f377bbec2c102a5c6ca7f5aee
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2023-04-15 04:17:36 -0300

    working colorized tags

--- src/pages/AnimeCard.jsx
+++ src/pages/AnimeCard.jsx
@@ -3,58 +3,69 @@ import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
-import MenuIcon from "@mui/icons-material/Menu";
 import styles from "../styles/AnimeCard.module.css";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
-
+import Chip from "@mui/material/Chip";
 
 export default function AnimeCard({ animes }) {
   const [animesToShow, setAnimesToShow] = useState([]);
-  const [text, setText] = useState("");
 
+  const [genres, setGenres] = useState([]);
+
+  const [genreColors, setGenreColors] = useState({});
+
+  const handleClick = (genre) => {
+    setGenreColors((prevState) => ({
+      ...prevState,
+      [genre]: prevState[genre] === "primary" ? 'default' : "primary",
+    }));
+  };
+
+  const [text, setText] = useState("");
   useEffect(() => {
     setText(text);
-
-    //console.log(animes);
-
-    const bla = animes.map(anime => anime.genres); //get all genres
-    console.log(bla);
-
-    const bla2 = bla.flatMap(arr => arr.map(obj => obj.name)); //put all genres in a single array
-    console.log(bla2);
-
-    const bla3 = bla2.filter((value, index) => bla2.indexOf(value) === index); //remove duplicate genres
-    console.log(bla3);
 
     setAnimesToShow(
       animes.filter((anime) =>
         anime.title.toLowerCase().includes(text.toLowerCase())
       )
     );
+    console.log(genreColors);
   }, [text]);
 
   return (
     <>
       <div className={styles.searchBarContainer}>
         <Paper className={styles.searchBar}>
-          <IconButton sx={{ p: "10px" }}>
-            <MenuIcon />
-          </IconButton>
-
           <InputBase
-            sx={{ ml: 1, flex: 1 }}
-            placeholder="Search Animes"
+            sx={{ ml: 1, flex: 1, pl: 2 }}
+            placeholder="Search Animes..."
             value={text}
+            //onChange={(e) => setText(e.target.value)}
             onChange={(e) => setText(e.target.value)}
-            onClick={() => setAnimesToShow(animes)}
-            onBlur={() => setAnimesToShow([])}
+            onClick={() => {
+              setAnimesToShow(animes);
+              setGenres([
+                ...new Set(
+                  animes.flatMap((anime) =>
+                    anime.genres.map((genre) => genre.name)
+                  )
+                ),
+              ]);
+            }}
+            //onBlur={() => {setAnimesToShow([]); setGenres([])}}
+          />
+
+          <Divider
+            sx={{ height: 25, mt: 1.1, mr: 0.5 }}
+            orientation="vertical"
           />
 
           <IconButton
             type="button"
             disabled
-            sx={{ p: "10px" }}
+            sx={{ p: "10px", pr: "18px" }}
             aria-label="search"
           >
             <SearchIcon />
@@ -62,6 +73,18 @@ export default function AnimeCard({ animes }) {
         </Paper>
 
         <div className={styles.autoCompleteContainer}>
+          <div className={styles.chipsContainer}>
+            {genres.map((genre) => (
+              <Chip
+                label={genre}
+                key={genre}
+                sx={{ mx: 0.3, mb: 0.8 }}
+                color={genreColors[genre] || "default"}
+                onClick={() => handleClick(genre)}
+              />
+            ))}
+          </div>
+
           {animesToShow.map((currentAnime) => (
             <AnimeCardSearch anime={currentAnime} key={currentAnime.title} />
           ))}
@@ -74,6 +97,7 @@ export default function AnimeCard({ animes }) {
 const AnimeCardSearch = (anime) => {
   let image = "";
   let title = "";
+  let genres = "";
   let score = "";
   let release = "";
 
@@ -85,12 +109,14 @@ const AnimeCardSearch = (anime) => {
 
     image = anime.anime.images.jpg.large_image_url;
     title = anime.anime.title;
+    genres = anime.anime.genres;
     score = anime.anime.score;
     release = anime.anime.broadcast.day;
   }
 
   return (
     <>
+      <Divider />
       <div className={styles.animeCardSearch}>
         <img
           className={styles.animeCardSearchImage}
@@ -107,14 +133,24 @@ const AnimeCardSearch = (anime) => {
             {title}
           </Typography>
 
-          <Typography color="text.secondary">Score: {score}</Typography>
+          <div className={styles.animeCardGenresContainer}>
+            {genres.map((genre, index) => (
+              <Typography color="text.secondary" key={genre.name}>
+                {genre.name}
+                {index === genres.length - 1 ? "" : ",\u00A0"}
+              </Typography>
+            ))}
+          </div>
+
+          <Typography color="text.secondary" sx={{ mt: "-4px" }}>
+            Score: {score}
+          </Typography>
 
           <Typography color="text.secondary" sx={{ mt: "-4px" }}>
             Release: {release}
           </Typography>
         </div>
       </div>
-      <Divider />
     </>
   );
-}
+};
